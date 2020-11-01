6c8c411c1252d706fd62b803f0301d2f4cd9b321
kjdeepak <kjdeepak4work@gmail.com>
2020-11-01 21:09:38 +0530

    Implement Cart service for state management logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/entities';
+import { CartService } from '../shared/services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,24 @@ import { Product } from '../shared/entities';
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.http.get<Product[]>('assets/data.json').subscribe(data => {
-      console.log(data);
       this.products = data;
     });
+  }
+
+  checkItemExistInCart(productId: number): boolean{
+    return this.cartService.checkItemExistInCart(productId);
+  }
+
+  onClickAddToCart(product: Product): void{
+    this.cartService.addItemToCart(product);
+  }
+
+  onClickRemoveItemFromCart(productId: number): void{
+    this.cartService.removeItemFromCart(productId);
   }
 
 }
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Product } from '../entities';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  // tslint:disable-next-line: variable-name
+  private readonly _cart: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
-  constructor() { }
+  readonly cart$ = this._cart.asObservable();
+
+  get cart(): Product[] {
+    return this._cart.getValue();
+  }
+  set cart(val: Product[]) {
+    this._cart.next(val);
+  }
+
+  addItemToCart(item: Product): void {
+    this.cart = [
+      ...this.cart,
+      {...item}
+    ];
+  }
+
+  removeItemFromCart(id: number): void{
+    this.cart = this.cart.filter(item => item.id !== id);
+  }
+
+  clearCart(): void {
+      this.cart = [];
+  }
+
+  checkItemExistInCart(id: number): boolean{
+    const product = this.cart.filter(item => item.id === id);
+    if (product.length){
+      return true;
+    }
+    return false;
+
+  }
+
+
 }
