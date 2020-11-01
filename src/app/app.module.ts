fdb2e19ab42916ca084a574ccf78ac2e6df64bac
kjdeepak <kjdeepak4work@gmail.com>
2020-11-01 14:22:12 +0530

    Implement basic folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CompanyComponent } from './company/company.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SideNavigationComponent } from './shared/components/side-navigation/side-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CompanyComponent,
+    ContactComponent,
+    HeaderComponent,
+    SideNavigationComponent
   ],
   imports: [
     BrowserModule,
