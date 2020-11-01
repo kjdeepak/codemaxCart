557aa6b943e74592dc4fb57c32d4b804b7cbf032
kjdeepak <kjdeepak4work@gmail.com>
2020-11-01 21:30:37 +0530

    Implement cart logic in header component

--- src/app/shared/entities.ts
+++ src/app/shared/entities.ts
@@ -2,7 +2,7 @@ export interface Product{
     id: 1;
     name: string;
     description: string;
-    price: number;
+    price: string;
     imageUrl: string;
     quantity: number;
 }
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -39,7 +39,6 @@ export class CartService {
       return true;
     }
     return false;
-
   }
 
 
