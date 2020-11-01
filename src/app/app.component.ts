e4a1a633af7bbe346556b3bd9218a696229b708f
kjdeepak <kjdeepak4work@gmail.com>
2020-11-01 13:41:09 +0530

    Implement bootstrap styling and Add json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'codemaxCart';
+  products: any = [];
+  constructor(private http: HttpClient){
+
+  }
+   ngOnInit(): void {
+    this.http.get('assets/data.json').subscribe(data => {
+      console.log(data);
+      this.products = data;
+    });
+     }
 }
